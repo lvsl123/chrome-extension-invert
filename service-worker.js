b224464563bd38be7b4914a78c040cf6ecb860d4
lvsl123 <72800908+lvsl123@users.noreply.github.com>
2024-01-19 02:55:27 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-chrome.webNavigation.onCompleted((tab) => {
-//chrome.action.onClicked.addListener((tab) => {
+/chrome.webNavigation.onCompleted((tab) => {
+chrome.action.onClicked.addListener((tab) => {
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     files: ["scripts/content.js"]
