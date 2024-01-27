f6fd86643278b7170a4f306bda5d681b70d386f8
lvsl123 <72800908+lvsl123@users.noreply.github.com>
2024-01-28 02:44:32 +0800

    img with subtab arent inverting solved

--- scripts/content.js
+++ scripts/content.js
@@ -2,20 +2,34 @@
  * File: chrome-extension-invert-main/scripts/content.js 
  */
 
+(function() {
+  let contentScriptHtmlElement;
+  let contentScriptImgElement;
+
+  for (contentScriptHtmlElement of document.getElementsByTagName("html")) {
+    console.log(contentScriptHtmlElement);
+    contentScriptHtmlElement.style.filter = "invert(1)";
+  }
+})();
+
+// ---- uninvert inverted images and video ----
 
 
-let contentScriptHtmlElement;
-let contentScriptImgElement;
+(function() {
+  let allItemsRaw = [...document.getElementsByTagName("img"), ...document.querySelectorAll("img[*]")];
+  let allItems = [];
+  for (let img of allItemsRaw) {
+    // note: it's not always true 
+    if (allItemsRaw.includes(img) && !allItems.include(img)) {
+      allItems.push(img);
+    }
+  }
 
-for (contentScriptHtmlElement of document.getElementsByTagName("html")) {
-  console.log(contentScriptHtmlElement);
-  contentScriptHtmlElement.style.filter = "invert(1)";
-}
+  for (let img of allItems) {
+    img.style.filter = "invert(1)";
+  }
+})();
 
-//uninvert inverted images and video
-for (let img of document.querySelectorAll("img")) {
-  img.style.filter = "invert(1)"
-}
 for (let video of document.querySelectorAll("video")) {
   video.style.filter = "invert(1)"
 }
